643ba326bbe8dbb641515e0aa09b80e29acba787
Ivan <molofeev.ivan@bk.ru>
2023-04-20 17:57:42 +0400

    1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using LAB2.Models;
+
+namespace Lab_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private static List<DatabaseModel> database = new List<DatabaseModel>();
+
+        public IActionResult Index()
+        {
+            return View(database);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DatabaseModel model)
+        {
+            model.ID = database.Count + 1;
+            database.Add(model);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var model = database.FirstOrDefault(m => m.ID == id);
+
+            if (model != null)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DatabaseModel model)
+        {
+            var modelToDelete = database.FirstOrDefault(m => m.ID == model.ID);
+
+            if (modelToDelete != null)
+            {
+                database.Remove(modelToDelete);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/DatabaseModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace LAB2.Models
+{
+    public class DatabaseModel
+    {
+        [Key]
+        [DisplayName("ID")]
+        public int ID { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Second Name")]
+        public string SecondName { get; set; }
+
+        [DisplayName("Age")]
+        public int Age { get; set; }
+
+        [DisplayName("Phone")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+    }
+}
